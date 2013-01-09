acaed2c3e10c0477c4317ab7670005f3404aea93
acmerfight <acmerfight@gmail.com>
2013-01-09 22:41:28 +0800

    about_new_style_classes.py

--- koans/about_new_style_classes.py
+++ koans/about_new_style_classes.py
@@ -18,15 +18,15 @@ class AboutNewStyleClasses(Koan):
         pass
 
     def test_new_style_classes_inherit_from_object_base_class(self):
-        self.assertEqual(____, issubclass(self.NewStyleClass, object))
-        self.assertEqual(____, issubclass(self.OldStyleClass, object))
+        self.assertEqual(True, issubclass(self.NewStyleClass, object))
+        self.assertEqual(False, issubclass(self.OldStyleClass, object))
         
     def test_new_style_classes_have_more_attributes(self):
-        self.assertEqual(__, len(dir(self.OldStyleClass)))
-        self.assertEqual(__, self.OldStyleClass.__doc__)
-        self.assertEqual(__, self.OldStyleClass.__module__)
+        self.assertEqual(2, len(dir(self.OldStyleClass)))
+        self.assertEqual("An old style class", self.OldStyleClass.__doc__)
+        self.assertEqual('koans.about_new_style_classes', self.OldStyleClass.__module__)
     
-        self.assertEqual(__, len(dir(self.NewStyleClass)))
+        self.assertEqual(18, len(dir(self.NewStyleClass)))
         # To examine the available attributes, run
         # 'dir(<Class name goes here>)'
         # from a python console
